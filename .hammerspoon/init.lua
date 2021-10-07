b040a604bd7127c79a5d34e06eedce83fc2cb5cc
Morgan McCauley <morgan@mccauley.co.nz>
2021-10-08 10:04:35 +1300

    Switch window focus via hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,8 +1,49 @@
 local VimMode = hs.loadSpoon('VimMode')
 local vim = VimMode:new()
 
+function switchDisplay()
+  local screen = hs.mouse.getCurrentScreen():next()
+
+  local windows = hs.fnutils.filter(
+    hs.window.allWindows(),
+    function (win)
+      return win:screen() == screen
+    end
+  )
+
+  local windowToFocus = #windows > 0 and windows[1] or hs.window.desktop()
+  windowToFocus:focus()
+
+  -- Move mouse to center of screen so that changing desktop works
+  hs.mouse.absolutePosition(screen:fullFrame().center)
+  hs.mouse.absolutePosition(screen:fullFrame().bottomright)
+
+  hs.alert.show('Focused', {}, screen, 0.5)
+end
+
+function dismissNotification()
+  hs.notify.withdrawAll()
+end
+
+hs.hotkey.bind('cmd', '`', switchDisplay)
+hs.hotkey.bind('cmd', 'q', dismissNotification)
+
 vim
   :disableForApp('Code')
   :disableForApp('MacVim')
   :disableForApp('zoom.us')
   :bindHotKeys({ enter = {{'ctrl'}, '\\'} })
+
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
