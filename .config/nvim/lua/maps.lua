840cd1ed4b9f0d6c791c332ea7332e13591b6652
Morgan McCauley <morgan@mccauley.co.nz>
2022-03-10 17:24:39 +1300

    Add maps

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -22,15 +22,18 @@ nmap('<C-t>', ':call MonkeyTerminalToggle()<Cr>', { silent = true, noremap = tru
 nmap('<C-n>', ':NvimTreeFindFileToggle<Cr>', { silent = true, noremap = true })
 nmap('<C-l>', '<C-w>l', { noremap = true })
 nmap('<C-h>', '<C-w>h', { noremap = true })
-nmap('<C-j>', '5j', { noremap = true })
-nmap('<C-k>', '5k', { noremap = true })
+nmap('<C-j>', '<C-w>j', { noremap = true })
+nmap('<C-k>', '<C-w>k', { noremap = true })
 
 nmap('<Esc>', ':noh | echo ""<Cr><Esc>', { silent = true, noremap = true })
 nmap('QQ', ':qall<CR>', { noremap = true })
 
 nmap('\\', '<cmd>HopWord<Cr>', { noremap = true, silent = true })
+nmap('<C-\\>', '<cmd>HopLine<Cr>', { noremap = true, silent = true })
+vmap('\\', '<cmd>HopWord<Cr>', { noremap = true, silent = true })
+vmap('<C-\\>', '<cmd>HopLine<Cr>', { noremap = true, silent = true })
 
-nmap('s', ':HopChar2<Cr>', { noremap = true, silent = true })
+nmap('s', ':HopChar1<Cr>', { noremap = true, silent = true })
 
 nmap('K', ':lua vim.lsp.buf.hover()<Cr>', { silent = true })
 
@@ -49,3 +52,12 @@ nmap("<C-_>", "<Plug>kommentary_line_default")
 vmap("<C-_>", "<Plug>kommentary_visual_default")
 
 nmap("<C-m>", ":MaximizerToggle!<Cr>")
+
+nmap('dl', 'd:HopLine<Cr>', { noremap = true, silent = true })
+nmap('yl', 'y:HopLine<Cr>', { noremap = true, silent = true })
+nmap('cl', 'c:HopLine<Cr>', { noremap = true, silent = true })
+
+nmap('t', ':HopChar1CurrentLine<Cr>', { noremap = true, silent = true })
+nmap('dt', 'd:HopChar1CurrentLine<Cr>', { noremap = true, silent = true })
+nmap('yt', 'y:HopChar1CurrentLine<Cr>', { noremap = true, silent = true })
+nmap('ct', 'c:HopChar1CurrentLine<Cr>', { noremap = true, silent = true })
