2339d8fe3650888f17360ec355579c1e0d444e02
Morgan McCauley <morgan@mccauley.co.nz>
2022-03-27 09:50:33 +1300

    Add luasnip

--- .config/nvim/lua/plugin-config/nvim-cmp.lua
+++ .config/nvim/lua/plugin-config/nvim-cmp.lua
@@ -1,4 +1,6 @@
 local cmp = require'cmp'
+local luasnip = require'luasnip'
+
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
@@ -8,10 +10,15 @@ local feedkey = function(key, mode)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
+require("luasnip.loaders.from_vscode").load()
+
 cmp.setup {
+  --[[ completion = {
+    autocomplete = true,
+  }, ]]
   snippet = {
     expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
   mapping = {
@@ -20,11 +27,33 @@ cmp.setup {
     ['<C-e>'] = cmp.mapping.scroll_docs(-4),
     ['<C-y>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
+    --[[ ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
-    },
+    }, ]]
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    --[[ ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif vim.fn["vsnip#available"](1) == 1 then
@@ -41,12 +70,13 @@ cmp.setup {
       elseif vim.fn["vsnip#jumpable"](-1) == 1 then
         feedkey("<Plug>(vsnip-jump-prev)", "")
       end
-    end, { "i", "s" }),
+    end, { "i", "s" }), ]]
   },
   sources = {
     { name = 'nvim_lsp', max_item_count = 10 },
     { name = 'path', max_item_count = 10 },
     { name = 'buffer', max_item_count = 10 },
+    { name = 'luasnip' },
   },
   --[[ experimental = {
     ghost_text = true,
