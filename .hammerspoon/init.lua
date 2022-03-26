db244bfeb5361defb3ac90fa3d41864a27ce8e42
Morgan McCauley <morgan@mccauley.co.nz>
2022-03-27 09:46:42 +1300

    Remove unused hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,30 +1,3 @@
-local VimMode = hs.loadSpoon('VimMode')
-local vim = VimMode:new()
-
-function switchDisplay()
-  local screen = hs.mouse.getCurrentScreen():next()
-
-  local windows = hs.fnutils.filter(
-    hs.window.allWindows(),
-    function (win)
-      return win:screen() == screen
-    end
-  )
-
-  local windowToFocus = #windows > 0 and windows[1] or hs.window.desktop()
-  windowToFocus:focus()
-
-  -- Move mouse to center of screen so that changing desktop works
-  hs.mouse.absolutePosition(screen:fullFrame().center)
-  hs.mouse.absolutePosition(screen:fullFrame().bottomright)
-
-  hs.alert.show('Focused', {}, screen, 0.5)
-end
-
-function dismissNotification()
-  hs.notify.withdrawAll()
-end
-
 function chromeToggleSearchBar()
   local currentApplication = hs.application.frontmostApplication()
   if currentApplication:name() == 'Google Chrome' then
@@ -39,15 +12,7 @@ function chromeToggleSearchBar()
   end
 end
 
-hs.hotkey.bind('cmd', '`', switchDisplay)
-hs.hotkey.bind('cmd', 'q', dismissNotification)
 hs.hotkey.bind('cmd', 'l', chromeToggleSearchBar)
-
-vim
-  :disableForApp('Code')
-  :disableForApp('MacVim')
-  :disableForApp('zoom.us')
-  :bindHotKeys({ enter = {{'ctrl'}, '\\'} })
 
 function reloadConfig(files)
     doReload = false
