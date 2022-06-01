4a01c709c92bdbb52004a31ca14e825c1b4374eb
Morgan McCauley <morgan@mccauley.co.nz>
2022-06-02 11:04:30 +1200

    Show diagnostic in float

--- .config/nvim/lua/plugins/config/which-key-nvim.lua
+++ .config/nvim/lua/plugins/config/which-key-nvim.lua
@@ -109,6 +109,7 @@ local code = {
   -- c = { ':Telescope commands<Cr>', 'Commands' },
   a = { ':Telescope lsp_code_actions<Cr>', 'Code actions' },
   d = { ':Telescope diagnostics bufnr=0<Cr>', 'File diagnostics' },
+  l = { ':lua vim.diagnostic.open_float()<Cr>', 'Show diagnostics for current line' },
   f = { ':lua vim.lsp.buf.formatting()<Cr>', 'Format' }
 }
 
