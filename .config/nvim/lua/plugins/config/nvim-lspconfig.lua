421f7314cb133b7266e48ffaeb23125f22b7d13e
Morgan McCauley <morgan@mccauley.co.nz>
2022-06-02 08:13:35 +1200

    Replace efm with eslint via LSP Installer

--- .config/nvim/lua/plugins/config/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/config/nvim-lspconfig.lua
@@ -2,38 +2,6 @@ local lspconfig = require('lspconfig')
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-local eslint = {
-  lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
-  lintStdin = true,
-  lintFormats = {"%f:%l:%c: %m"},
-  lintIgnoreExitCode = true,
-  formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename ${INPUT}",
-  formatStdin = true
-}
-
-lspconfig.efm.setup {
-  capabilities = capabilities,
-  init_options = {
-    documentFormatting = true
-  },
-  on_attach = function()
-    vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
-  end,
-  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-  root_dir = function(fname)
-    return lspconfig.util.root_pattern(".eslintrc.js", ".git")(fname);
-  end,
-  settings = {
-    rootMarkers = {".eslintrc.js", ".git/"},
-    languages = {
-      javascript = {eslint},
-      typescript = {eslint},
-      javascriptreact = {eslint},
-      typescriptreact = {eslint},
-    }
-  }
-}
-
 lspconfig.tsserver.setup {
   capabilities = capabilities,
   init_options = {
