9ed6a20b98b6004b58591825ac8e049a76765042
Morgan McCauley <morgan@mccauley.co.nz>
2022-07-13 12:12:19 +1200

    Update lsp config

--- .config/nvim/lua/plugins/config/nvim-lsp-installer.lua
+++ .config/nvim/lua/plugins/config/nvim-lsp-installer.lua
@@ -1,19 +1,8 @@
-local lsp_installer = require("nvim-lsp-installer")
-
-lsp_installer.on_server_ready(function(server)
-    local opts = {}
-
-    if server.name == "sumneko_lua" then
-        opts = {
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        globals = {'vim'}
-                    }
-                }
-            }
-        }
-    end
-
-    server:setup(opts)
-end)
+require("nvim-lsp-installer").setup {
+  ensure_installed = {
+    'tsserver',
+    'sumneko_lua',
+    'rust_analyzer',
+    'eslint'
+  }
+}
--- .config/nvim/lua/plugins/config/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/config/nvim-lspconfig.lua
@@ -10,7 +10,7 @@ lspconfig.tsserver.setup {
     },
   },
   on_attach = function(client)
-    client.resolved_capabilities.document_formatting = false
+    client.server_capabilities.document_formatting = false
   end,
   flags = {
     debounce_text_changes = 150,
@@ -24,32 +24,13 @@ lspconfig.rust_analyzer.setup {
   }
 }
 
---[[ local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
 lspconfig.sumneko_lua.setup {
   capabilities = capabilities,
-  cmd = {
-    vim.fn.getenv('HOME') .. '/lua-language-server/bin/macOS/lua-language-server',
-    '-E',
-    vim.fn.getenv('HOME') ..'/lua-language-server/main.lua'
-  };
   settings = {
     Lua = {
-      runtime = {
-        version = 'LuaJIT',
-        path = runtime_path,
-      },
       diagnostics = {
-        globals = {'vim', 'hs'},
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-} ]]
+        globals = {'vim'}
+      }
+    }
+  }
+}
