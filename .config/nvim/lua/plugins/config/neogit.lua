5e4fb4c78ac642a147efce7a8223851f34021671
Morgan McCauley <morgan@mccauley.co.nz>
2022-08-09 14:37:06 +1200

    neogit map updates

--- .config/nvim/lua/plugins/config/neogit.lua
+++ .config/nvim/lua/plugins/config/neogit.lua
@@ -1,7 +1,14 @@
 require('neogit').setup {
   disable_commit_confirmation = true,
   disable_insert_on_commit = false,
+  kind = 'split',
   integrations = {
     diffview = true
   },
+  mappings = {
+    status = {
+      ['p'] = 'PushPopup',
+      ['l'] = 'PullPopup'
+    }
+  }
 }
