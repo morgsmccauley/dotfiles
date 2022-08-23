15de693de37617967cf09fe46edf26bb31acb30f
Morgan McCauley <morgan@mccauley.co.nz>
2022-08-24 07:49:25 +1200

    fix dap-repl maps and startinsert

--- .config/nvim/lua/autocommand.lua
+++ .config/nvim/lua/autocommand.lua
@@ -35,16 +35,16 @@ vim.api.nvim_create_autocmd({'VimResized'}, {
 
 vim.api.nvim_create_autocmd({'FileType'}, {
     pattern = {'dap-repl'},
-    command = 'startinsert!'
-})
-
-vim.api.nvim_create_autocmd({'FileType'}, {
-    pattern = {'dap-repl'},
     callback = function(t)
-        vim.api.nvim_buf_set_keymap(t.buf, 't', '<C-h>', [[<C-\><C-n><C-W><C-h>]], { noremap = true })
-        vim.api.nvim_buf_set_keymap(t.buf, 't', '<C-\\>', [[<C-\><C-n>]], { noremap = true })
-        vim.api.nvim_buf_set_keymap(t.buf, 't', '<C-j>', [[<C-\><C-n><C-W><C-j>]], { noremap = true })
-        vim.api.nvim_buf_set_keymap(t.buf, 't', '<C-k>', [[<C-\><C-n><C-W><C-k>]], { noremap = true })
+        vim.api.nvim_buf_set_keymap(t.buf, 'i', '<C-h>', [[<C-\><C-n><C-W><C-h>]], { noremap = true })
+        vim.api.nvim_buf_set_keymap(t.buf, 'i', '<C-\\>', [[<C-\><C-n>]], { noremap = true })
+        vim.api.nvim_buf_set_keymap(t.buf, 'i', '<C-j>', [[<C-\><C-n><C-W><C-j>]], { noremap = true })
+        vim.api.nvim_buf_set_keymap(t.buf, 'i', '<C-k>', [[<C-\><C-n><C-W><C-k>]], { noremap = true })
+
+        vim.api.nvim_create_autocmd({'BufEnter'}, {
+            buffer = t.buf,
+            command = 'startinsert!'
+        })
     end
 })
 
