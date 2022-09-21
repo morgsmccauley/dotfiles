2719fa897b090d92a87b81f06e68234a32602edb
Morgan McCauley <morgan@mccauley.co.nz>
2022-09-21 15:04:55 +1200

    fix gitsigns highlights and remove default

--- .config/nvim/lua/plugins/config/gitsigns.lua
+++ .config/nvim/lua/plugins/config/gitsigns.lua
@@ -1,15 +1,9 @@
 require('gitsigns').setup {
     signs = {
-        add = {hl = 'DiffAdd', text = '▌', numhl = 'GitSignsAddNr'},
-        change = {hl = 'DiffChange', text = '▌', numhl = 'GitSignsChangeNr'},
-        delete = {hl = 'DiffDelete', text = '_', numhl = 'GitSignsDeleteNr'},
-        topdelete = {hl = 'DiffDelete', text = '‾', numhl = 'GitSignsDeleteNr'},
-        changedelete = {hl = 'DiffChange', text = '~', numhl = 'GitSignsChangeNr'}
+        add = {text = '▌'},
+        change = {text = '▌'},
     },
-    numhl = false,
     watch_gitdir = {
         interval = 100
     },
-    sign_priority = 5,
-    status_formatter = nil -- Use default
 }
