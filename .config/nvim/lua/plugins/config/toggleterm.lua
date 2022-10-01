6733da1815af846a5bbc23c2f9d7c33d01c68cdc
Morgan McCauley <morgan@mccauley.co.nz>
2022-10-01 20:17:59 +1300

    hack to make all toggleterms equal width

--- .config/nvim/lua/plugins/config/toggleterm.lua
+++ .config/nvim/lua/plugins/config/toggleterm.lua
@@ -1,6 +1,6 @@
-local git = require 'lib.git'
+-- local git = require 'lib.git'
 
-local git_editor = git.get_editor()
+-- local git_editor = git.get_editor()
 
 require 'toggleterm'.setup {
   shade_terminals = false,
@@ -8,21 +8,21 @@ require 'toggleterm'.setup {
   open_mapping = [[<C-t>]],
   start_in_insert = true,
   persist_size = false,
-  env = {
-    GIT_EDITOR = git_editor,
-    GIT_SEQUENCE_EDITOR = git_editor
-  },
-  size = function()
-    return vim.o.columns / vim.fn.tabpagewinnr(vim.fn.tabpagenr(), '$')
-  end,
+  -- env = {
+  --   GIT_EDITOR = git_editor,
+  --   GIT_SEQUENCE_EDITOR = git_editor
+  -- },
   on_open = function(t)
     vim.wo.winfixwidth = false
+
+    -- hack
+    vim.api.nvim_command [[wincmd =]]
 
     vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-h>', [[<C-\><C-n><C-W><C-h>]], { noremap = true })
     vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-\\>', [[<C-\><C-n>]], { noremap = true })
     vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-j>', [[<C-\><C-n><C-W><C-j>]], { noremap = true })
     vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-k>', [[<C-\><C-n><C-W><C-k>]], { noremap = true })
-    vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-x>', [[<C-\><C-n>:q!<Cr>]], { noremap = true })
+    vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-q>', [[<C-\><C-n>:q!<Cr>]], { noremap = true })
 
     vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-n>', [[<C-\><C-n>:NvimTreeToggle<Cr>]], { noremap = true })
   end,
