16b26a37de3d4d8504570f3f463ea24611c5b079
Morgan McCauley <morgan@mccauley.co.nz>
2022-10-05 08:49:36 +1300

    fix format on save by making it sync

--- .config/nvim/lua/plugins/config/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/config/nvim-lspconfig.lua
@@ -28,7 +28,7 @@ lspconfig.sumneko_lua.setup {
   on_attach = function(_, bufnr)
     vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
       buffer = bufnr,
-      callback = vim.lsp.buf.formatting_sync,
+      callback = function() vim.lsp.buf.format({ async = false }) end,
     })
   end
 }
