a325597a846f678e2fc6735371367c00e01143a5
Morgan McCauley <morgan@mccauley.co.nz>
2022-10-26 12:30:14 +1300

    add placeholder 'jump to window' via leap

--- .config/nvim/lua/plugins/config/leap.lua
+++ .config/nvim/lua/plugins/config/leap.lua
@@ -42,4 +42,22 @@ function M.leap_to_line()
 	}
 end
 
+function M.leap_to_window()
+	local target_windows = require('leap.util').get_enterable_windows()
+	local targets = {}
+	for _, win in ipairs(target_windows) do
+		local wininfo = vim.fn.getwininfo(win)[1]
+		local pos = { wininfo.topline, 1 } -- top/left corner
+		table.insert(targets, { pos = pos, wininfo = wininfo })
+	end
+
+	require('leap').leap {
+		target_windows = target_windows,
+		targets = targets,
+		action = function(target)
+			vim.api.nvim_set_current_win(target.wininfo.winid)
+		end
+	}
+end
+
 return M
