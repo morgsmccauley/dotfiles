84a43ad4e0a7b26a8cc1a5bb9e6a35b20b0e3cca
Morgan McCauley <morgan@mccauley.co.nz>
2022-10-26 12:29:23 +1300

    update maps

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -15,6 +15,11 @@ local function vmap(lhs, rhs, opts)
   vim.api.nvim_set_keymap('v', lhs, rhs, opts)
 end
 
+local function cmap(lhs, rhs, opts)
+  opts = opts or {}
+  vim.api.nvim_set_keymap('c', lhs, rhs, opts)
+end
+
 local function smap(lhs, rhs, opts)
   opts = opts or {}
   vim.api.nvim_set_keymap('s', lhs, rhs, opts)
@@ -23,9 +28,9 @@ end
 nmap('j', 'gj', { silent = true, noremap = true })
 nmap('k', 'gk', { silent = true, noremap = true })
 
-nmap('<C-S-t>', ':ToggleTermToggleAll<Cr>', { silent = true, noremap = true })
+nmap('<C-S-t>', '<Cmd>ToggleTermToggleAll<Cr>', { silent = true, noremap = true })
 nmap('<C-t>', '<Cmd>exe v:count1 . "ToggleTerm"<Cr>', { silent = true, noremap = true })
-nmap('<C-n>', ':NvimTreeFindFileToggle<Cr>', { silent = true, noremap = true })
+nmap('<C-n>', '<Cmd>NvimTreeFindFileToggle<Cr>', { silent = true, noremap = true })
 nmap('<C-l>', '<C-w>l', { noremap = true })
 nmap('<C-h>', '<C-w>h', { noremap = true })
 nmap('<C-j>', '<C-w>j', { noremap = true })
@@ -42,23 +47,23 @@ imap('<Backspace>', 'luasnip#jumpable(-1) ? "<Plug>luasnip-jump-prev" : "<Backsp
 smap('<Backspace>', 'luasnip#jumpable(-1) ? "<Plug>luasnip-jump-prev" : "<Backspace>"',
   { noremap = true, silent = true, expr = true })
 
-nmap('<C-g>', ':Neogit<Cr>', { noremap = true, silent = true })
+nmap('<C-g>', '<Cmd>Neogit<Cr>', { noremap = true, silent = true })
 
 imap('<C-a>', '<C-o>^', { noremap = true, silent = true })
 imap('<C-e>', '<C-o>$', { noremap = true, silent = true })
 
-nmap('<C-q>', ':q<Cr>', { noremap = true })
-nmap('<C-s>', ':w<Cr>', { noremap = true, silent = true })
+nmap('<C-q>', '<Cmd>silent q<Cr>', { noremap = true, silent = true })
+nmap('<C-s>', '<Cmd>w<Cr>', { noremap = true, silent = true })
 
-nmap('<Esc>', ':noh | echo ""<Cr><Esc>', { silent = true, noremap = true })
-nmap('QQ', ':qall<CR>', { noremap = true })
+nmap('<Esc>', '<Cmd>noh | echo ""<Cr>', { silent = true, noremap = true })
+nmap('QQ', '<Cmd>qall<CR>', { noremap = true })
 
-nmap('K', ':lua vim.lsp.buf.hover()<Cr>', { silent = true })
+nmap('K', '<Cmd>lua vim.lsp.buf.hover()<Cr>', { silent = true })
 
-nmap('gd', ':Telescope lsp_definitions<Cr>', { silent = true })
-nmap('gD', '<cmd>lua vim.lsp.buf.type_definition()<CR>', { silent = true })
-nmap('gi', ':Telescope lsp_implementations<Cr>', { silent = true })
-nmap('gr', ':Telescope lsp_references<Cr>', { silent = true })
+nmap('gd', '<Cmd>silent Telescope lsp_definitions<Cr>', { silent = true })
+nmap('gD', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', { silent = true })
+nmap('gi', '<Cmd>Telescope lsp_implementations<Cr>', { silent = true })
+nmap('gr', '<Cmd>Telescope lsp_references<Cr>', { silent = true })
 
 nmap('<C-e>', '5<C-e>', { noremap = true, silent = true })
 nmap('<C-y>', '5<C-y>', { noremap = true, silent = true })
@@ -67,10 +72,12 @@ imap('<Tab>', 'pumvisible() ? \"\\<C-n>\" : \"\\<Tab>\"', { expr = true, noremap
 imap('<S-Tab>', 'pumvisible() ? \"\\<C-p>\" : \"\\<S-Tab>\"', { expr = true, noremap = true })
 
 nmap('<C-/>', '<Plug>(comment_toggle_linewise_current)')
-vmap('<C-/>', '<Plug>(comment_toggle_linewise_visual)')
+vmap('<C-/>', '<Plug>(comment_toggle_blockwise_visual)')
 
-nmap('<C-m>', ':MaximizerToggle!<Cr>', { noremap = true, silent = true })
+nmap('<C-m>', '<Cmd>MaximizerToggle!<Cr>', { noremap = true, silent = true })
 
-nmap('dl', 'dV:lua require(\'plugins/config/leap.lua\').leap_to_line()<Cr>', { noremap = true, silent = true })
-nmap('yl', 'yV:lua require(\'plugins/config/leap.lua\').leap_to_line()<Cr>', { noremap = true, silent = true })
-nmap('cl', 'cV:lua require(\'plugins/config/leap.lua\').leap_to_line()<Cr>', { noremap = true, silent = true })
+nmap('dl', 'dV:lua require(\'plugins/config/leap\').leap_to_line()<Cr>', { noremap = true, silent = true })
+nmap('yl', 'yV:lua require(\'plugins/config/leap\').leap_to_line()<Cr>', { noremap = true, silent = true })
+nmap('cl', 'cV:lua require(\'plugins/config/leap\').leap_to_line()<Cr>', { noremap = true, silent = true })
+
+cmap('<C-a>', '<C-b>', { noremap = true })
