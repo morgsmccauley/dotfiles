80e31507d9655828e9f3ccdb3d30fe8ea4c67d99
Morgan McCauley <morgan@mccauley.co.nz>
2022-10-26 12:51:17 +1300

    update toggleterm maps

--- .config/nvim/lua/plugins/config/toggleterm.lua
+++ .config/nvim/lua/plugins/config/toggleterm.lua
@@ -19,11 +19,9 @@ require 'toggleterm'.setup {
     vim.api.nvim_command [[wincmd =]]
 
     vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-h>', [[<C-\><C-n><C-W><C-h>]], { noremap = true })
-    vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-\\>', [[<C-\><C-n>]], { noremap = true })
+    vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<Esc>', [[<C-\><C-n>]], { noremap = true })
     vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-j>', [[<C-\><C-n><C-W><C-j>]], { noremap = true })
     vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-k>', [[<C-\><C-n><C-W><C-k>]], { noremap = true })
     vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-q>', [[<C-\><C-n>:q!<Cr>]], { noremap = true })
-
-    vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-n>', [[<C-\><C-n>:NvimTreeToggle<Cr>]], { noremap = true })
   end,
 }
