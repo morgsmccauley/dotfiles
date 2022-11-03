84738e8f9398538dbd9fb95a3416d0891048ea16
Morgan McCauley <morgan@mccauley.co.nz>
2022-11-03 20:34:38 +1300

    Update cmp styling

--- .config/nvim/lua/plugins/config/nvim-cmp.lua
+++ .config/nvim/lua/plugins/config/nvim-cmp.lua
@@ -4,10 +4,10 @@ local lspkind = require 'lspkind'
 
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
 end
 
-require("luasnip.loaders.from_vscode").lazy_load()
+require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup {
   --[[ completion = {
@@ -24,7 +24,7 @@ cmp.setup {
     ['<C-e>'] = cmp.mapping.scroll_docs(-4),
     ['<C-y>'] = cmp.mapping.scroll_docs(4),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -34,9 +34,9 @@ cmp.setup {
       else
         fallback()
       end
-    end, { "i", "s" }),
+    end, { 'i', 's' }),
 
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
@@ -44,10 +44,10 @@ cmp.setup {
       else
         fallback()
       end
-    end, { "i", "s" }),
+    end, { 'i', 's' }),
   },
   sources = {
-    { name = 'nvim_lsp', max_item_count = 10 },
+    { name = 'nvim_lsp' },
     { name = 'path', max_item_count = 10 },
     { name = 'buffer', max_item_count = 10 },
     { name = 'luasnip' },
@@ -56,8 +56,13 @@ cmp.setup {
     format = lspkind.cmp_format({
       mode = 'symbol_text'
     })
-  }
-  --[[ experimental = {
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  complete_opt = 'menu,menuone,noinsert',
+  experimental = {
     ghost_text = true,
-  } ]]
+  }
 }
