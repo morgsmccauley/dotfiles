89b8ae9f7ad493ff9fe353fdd79a2655591be8da
Morgan McCauley <morgan@mccauley.co.nz>
2023-03-05 20:29:36 +1300

    Ensure only one of tree/symbols is open

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,9 +1,21 @@
+local is_open = false
+
 return {
     'kyazdani42/nvim-tree.lua',
     keys = {
         {
             '<C-n>',
-            '<Cmd>NvimTreeFindFileToggle<Cr>',
+            function()
+                if not is_open then
+                    vim.api.nvim_exec_autocmds('User', { pattern = 'NvimTreeOpen' })
+                    require('nvim-tree.api').tree.open({ find_file = true })
+                    is_open = true
+                else
+                    require('nvim-tree.api').tree.close()
+                    is_open = false
+                end
+            end
+            ,
             { silent = true, noremap = true }
         }
     },
@@ -17,6 +29,16 @@ return {
         vim.fn.sign_define('NvimTreeSignInformation', { texthl = 'NvimTreeSignError', text = '' })
     end,
     config = function()
+        vim.api.nvim_create_autocmd({ 'User' }, {
+            pattern = { 'SymbolsOutlineOpen' },
+            callback = function()
+                if is_open then
+                    require('nvim-tree.api').tree.close()
+                    is_open = false
+                end
+            end
+        })
+
         require 'nvim-tree'.setup {
             hijack_netrw = false,
             sync_root_with_cwd = true,
@@ -84,16 +106,5 @@ return {
                 }
             }
         }
-
-        --[[ vim.cmd 'hi NvimTreeGitDirty NONE'
-    vim.cmd 'hi NvimTreeGitStaged NONE'
-    vim.cmd 'hi NvimTreeGitMerge NONE'
-    vim.cmd 'hi NvimTreeGitRenamed NONE'
-    vim.cmd 'hi NvimTreeGitNew NONE'
-    vim.cmd 'hi NvimTreeGitDeleted NONE' ]]
-        --[[ vim.fn.sign_define('NvimTreeSignError', { texthl = 'NvimTreeSignError', text = '' })
-    vim.fn.sign_define('NvimTreeSignHint', { texthl = 'NvimTreeSignError', text = '' })
-    vim.fn.sign_define('NvimTreeSignWarning', { texthl = 'NvimTreeSignError', text = '' })
-    vim.fn.sign_define('NvimTreeSignInformation', { texthl = 'NvimTreeSignError', text = '' }) ]]
     end
 }
--- nvim/lua/plugins/symbols-outline.lua
+++ nvim/lua/plugins/symbols-outline.lua
@@ -1,8 +1,20 @@
+local is_open = false
+
 return {
   'simrat39/symbols-outline.nvim',
   keys = {
     {
       '<C-p>',
+      function()
+        if not is_open then
+          vim.api.nvim_exec_autocmds('User', { pattern = 'SymbolsOutlineOpen' })
+          vim.api.nvim_command('SymbolsOutlineOpen')
+          is_open = true
+        else
+          vim.api.nvim_command('SymbolsOutlineClose')
+          is_open = false
+        end
+      end,
       '<Cmd>SymbolsOutline<Cr>',
       { silent = true, noremap = true }
     }
@@ -10,6 +22,16 @@ return {
   config = function()
     require('symbols-outline').setup({
       position = 'left'
+    })
+
+    vim.api.nvim_create_autocmd({ 'User' }, {
+      pattern = { 'NvimTreeOpen' },
+      callback = function()
+        if is_open then
+          vim.api.nvim_command('SymbolsOutlineClose')
+          is_open = false
+        end
+      end
     })
   end,
 }
