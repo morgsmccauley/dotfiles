b19527cc4e3c5d3691eae33275dd7ea08488628e
Morgan McCauley <morgan@mccauley.co.nz>
2023-03-06 14:18:27 +1300

    Make C-j/k terminal mappings fzf aware

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -19,18 +19,42 @@ return {
         vim.o.bufhidden = 'hide'
         vim.wo.winfixwidth = false
 
-        -- hack
+        -- keep terminal equal size
         vim.api.nvim_command [[wincmd =]]
 
         vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<Esc>', [[<C-\><C-n>]], { noremap = true })
 
         vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-h>', [[<C-\><C-n><C-W><C-h>]], { noremap = true })
-        vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-j>', [[<C-\><C-n><C-W><C-j>]], { noremap = true })
-        vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-k>', [[<C-\><C-n><C-W><C-k>]], { noremap = true })
-        -- vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-l>', [[<C-\><C-n><C-W><C-l>]], { noremap = true })
+
+        -- this looks for fzf in every process so will not do as expected if fzf is running in a different terminal
+        vim.keymap.set(
+          't',
+          '<C-j>',
+          function()
+            local isFzf = vim.fn.system('ps -o state= -o comm= | grep -iE \'^[^TXZ ]+ +(\\S+\\/)?fzf$\'') ~= ''
+            if isFzf then
+              return [[<C-j>]]
+            else
+              return [[<C-\><C-n><C-w><C-j>]]
+            end
+          end,
+          { buffer = t.bufnr, expr = true }
+        )
+        vim.keymap.set(
+          't',
+          '<C-k>',
+          function()
+            local isFzf = vim.fn.system('ps -o state= -o comm= | grep -iE \'^[^TXZ ]+ +(\\S+\\/)?fzf$\'') ~= ''
+            if isFzf then
+              return [[<C-k>]]
+            else
+              return [[<C-\><C-n><C-w><C-k>]]
+            end
+          end,
+          { buffer = t.bufnr, expr = true }
+        )
 
         vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-q>', [[<C-\><C-n><Cmd>q!<Cr>]], { noremap = true })
-        -- vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<C-t>', [[<C-\><C-n><Cmd>hide<Cr>]], { noremap = true })
 
         -- perform relevant action rather than sending keycodes
         vim.api.nvim_buf_set_keymap(t.bufnr, 't', '<S-space>', ' ', { noremap = true })
