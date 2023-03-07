206c5f647ac8bbbe2b55da5b354265c4bf700b14
Morgan McCauley <morgan@mccauley.co.nz>
2023-03-07 14:51:46 +1300

    Bootstrap lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,20 @@
 require('impatient')
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-require('plugins')
 require('options')
 require('maps')
+require('plugins')
 require('highlights')
 require('autocommand')
 
