2f7b2863d507bf2eacc6dcc7cb74ffdf342689c5
Morgan McCauley <morgan@mccauley.co.nz>
2023-03-13 11:09:02 +1300

    Lazy load illuminate

--- nvim/lua/plugins/illuminate.lua
+++ nvim/lua/plugins/illuminate.lua
@@ -1,5 +1,6 @@
 return {
   'RRethy/vim-illuminate',
+  event = 'VeryLazy',
   init = function()
     vim.api.nvim_set_hl(0, 'IlluminatedWordText', { link = 'Visual' })
     vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { link = 'Visual' })
