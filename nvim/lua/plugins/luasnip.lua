5421bb13a499e6309fd3b7ed50ae6075b6117277
Morgan McCauley <morgan@mccauley.co.nz>
2023-03-16 20:07:12 +1300

    Fix luasnip keymaps

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,15 +1,33 @@
 return {
   'L3MON4D3/LuaSnip',
-  event = 'InsertEnter',
   dependencies = {
     'rafamadriz/friendly-snippets',
   },
-  init = function()
-    vim.keymap.set('i', '<C-l>', '<Plug>luasnip-expand-or-jump', { noremap = true, silent = true })
-    vim.keymap.set('s', '<C-l>', '<Plug>luasnip-jump-next', { noremap = true, silent = true })
-    vim.keymap.set('i', '<C-h>', '<Plug>luasnip-jump-prev', { noremap = true, silent = true })
-    vim.keymap.set('s', '<C-h>', '<Plug>luasnip-jump-prev', { noremap = true, silent = true })
-  end,
+  event = 'InsertEnter',
+  keys = {
+    {
+      '<C-l>',
+      function()
+        local luasnip = require 'luasnip'
+        if luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        end
+      end,
+      mode = { 'i', 's' },
+      { noremap = true, silent = true }
+    },
+    {
+      '<C-h>',
+      function()
+        local luasnip = require 'luasnip'
+        if luasnip.jumpable( -1) then
+          luasnip.jump( -1)
+        end
+      end,
+      mode = { 'i', 's' },
+      { noremap = true, silent = true }
+    }
+  },
   config = function()
     require('luasnip').filetype_extend('typescript', { 'javascript' })
     require('luasnip.loaders.from_snipmate').lazy_load({ paths = { '~/.config/nvim/snippets' } })
