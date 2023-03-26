b3581a842c30148384b4da3c36d981a754870cab
Morgan McCauley <morgan@mccauley.co.nz>
2023-03-26 19:39:57 +1300

    Tidy cmp mappings

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -20,8 +20,7 @@ return {
           cmp.setup.cmdline(':', {
             -- TODO reverse order
             mapping = cmp.mapping.preset.cmdline({
-              ['<CR>'] = {
-                -- TODO auto CR after selecting
+              ['<C-y>'] = {
                 c = cmp.mapping.confirm({ select = true })
               },
               ['<C-j>'] = {
@@ -56,11 +55,6 @@ return {
       local luasnip = require 'luasnip'
       local lspkind = require 'lspkind'
 
-      local has_words_before = function()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-      end
-
       require('luasnip.loaders.from_vscode').lazy_load()
 
       cmp.setup {
@@ -78,7 +72,7 @@ return {
           ['<C-k>'] = cmp.mapping.select_prev_item(),
           ['<C-j>'] = cmp.mapping.select_next_item(),
           -- currently this is used to go to end of line
-          ['<C-e>'] = cmp.mapping.close(),
+          ['<C-e>'] = cmp.mapping.abort(),
           ['<C-y>'] = cmp.mapping.confirm({
             behaviour = cmp.ConfirmBehavior.Replace,
             select = true
@@ -91,22 +85,6 @@ return {
           }),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<Tab>'] = cmp.mapping.disable
-          --[[ ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }), ]]
         },
         sources = {
           { name = 'copilot' },
