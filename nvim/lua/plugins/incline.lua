b1efab6605953cd21722674613f6f05c3ee1ec64
Morgan McCauley <morgan@mccauley.co.nz>
2023-04-18 20:07:35 +1200

    Show icon in incline

--- nvim/lua/plugins/incline.lua
+++ nvim/lua/plugins/incline.lua
@@ -3,6 +3,15 @@ return {
   event = 'VeryLazy',
   config = function()
     require('incline').setup({
+      render = function(props)
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+        local icon, color = require('nvim-web-devicons').get_icon_color(filename)
+        return {
+          { icon,    guifg = color },
+          { ' ' },
+          { filename },
+        }
+      end,
       window = {
         margin = {
           horizontal = 0
