03d9dcf2737a9bfaf547ca5c8f60cadba6564d60
Morgan McCauley <morgan@mccauley.co.nz>
2023-04-21 11:27:12 +1200

    Move cmp_lsp to cmp config

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,124 +1,132 @@
 return {
-  {
+  'hrsh7th/nvim-cmp',
+  event = 'InsertEnter',
+  dependencies = {
+    'onsails/lspkind.nvim',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-buffer',
+    'saadparwaiz1/cmp_luasnip',
     'hrsh7th/cmp-nvim-lsp',
-  },
-  {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      'onsails/lspkind.nvim',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-buffer',
-      'saadparwaiz1/cmp_luasnip',
-      {
-        'hrsh7th/cmp-cmdline',
-        keys = {
-          { ';', ':', { noremap = true, silent = true } }
-        },
-        config = function()
-          local cmp = require 'cmp'
-          cmp.setup.cmdline(':', {
-            -- TODO reverse order
-            mapping = cmp.mapping.preset.cmdline({
-              ['<Cr>'] = {
-                c = cmp.mapping.confirm({ select = true })
-              },
-              ['<C-j>'] = {
-                c = cmp.mapping.select_next_item()
-              },
-              ['<C-k>'] = {
-                c = cmp.mapping.select_prev_item()
-              }
-            }),
-            sources = cmp.config.sources(
+    {
+      'hrsh7th/cmp-cmdline',
+      keys = {
+        { ';', ':', { noremap = true, silent = true } }
+      },
+      config = function()
+        local cmp = require 'cmp'
+        cmp.setup.cmdline(':', {
+          -- TODO reverse order
+          mapping = cmp.mapping.preset.cmdline({
+            ['<Cr>'] = {
+              c = cmp.mapping.confirm({ select = true })
+            },
+            ['<C-j>'] = {
+              c = cmp.mapping.select_next_item()
+            },
+            ['<C-k>'] = {
+              c = cmp.mapping.select_prev_item()
+            }
+          }),
+          sources = cmp.config.sources(
+            {
+              { name = 'path' }
+            },
+            {
               {
-                { name = 'path' }
-              },
-              {
-                {
-                  name = 'cmdline',
-                  option = {
-                    ignore_cmds = { 'Man', '!' }
-                  }
+                name = 'cmdline',
+                option = {
+                  ignore_cmds = { 'Man', '!' }
                 }
               }
-            )
-          })
-        end
-      },
-      {
-        'kristijanhusak/vim-dadbod-completion',
-        config = function()
-          vim.api.nvim_create_autocmd({ 'FileType' }, {
-            pattern = { 'sql', 'mysql', 'pgsql', 'plsql' },
-            callback = function()
-              require('cmp').setup.buffer({ sources = { { name = 'vim-dadbod-completion' } } })
-            end
-          })
-        end
-      }
+            }
+          )
+        })
+      end
     },
-    init = function()
-      vim.api.nvim_set_hl(0, 'CmpItemKindCopilot', { fg = '#6CC644' })
-    end,
-    config = function()
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      local lspkind = require 'lspkind'
+    {
+      'kristijanhusak/vim-dadbod-completion',
+      config = function()
+        vim.api.nvim_create_autocmd({ 'FileType' }, {
+          pattern = { 'sql', 'mysql', 'pgsql', 'plsql' },
+          callback = function()
+            require('cmp').setup.buffer({ sources = { { name = 'vim-dadbod-completion' } } })
+          end
+        })
+      end
+    }
+  },
+  init = function()
+    vim.api.nvim_set_hl(0, 'CmpItemKindCopilot', { fg = '#6CC644' })
+  end,
+  config = function()
+    local cmp = require 'cmp'
+    local luasnip = require 'luasnip'
+    local lspkind = require 'lspkind'
+    local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local lspconfig = require('lspconfig')
 
-      require('luasnip.loaders.from_vscode').lazy_load()
+    require('luasnip.loaders.from_vscode').lazy_load()
 
-      cmp.setup {
-        --[[ completion = {
+    lspconfig.util.default_config = vim.tbl_extend(
+      'force',
+      lspconfig.util.default_config,
+      {
+        capabilities = capabilities,
+      }
+    )
+
+
+
+    cmp.setup {
+      --[[ completion = {
           autocomplete = true,
         }, ]]
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = {
-          --[[ ['<C-p>'] = cmp.mapping.select_prev_item(),
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = {
+        --[[ ['<C-p>'] = cmp.mapping.select_prev_item(),
           ['<C-n>'] = cmp.mapping.select_next_item(), ]]
-          ['<C-k>'] = cmp.mapping.select_prev_item(),
-          ['<C-j>'] = cmp.mapping.select_next_item(),
-          -- currently this is used to go to end of line
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<C-y>'] = cmp.mapping.confirm({
-            behaviour = cmp.ConfirmBehavior.Replace,
-            select = true
-          }),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-          ['<CR>'] = cmp.mapping.confirm({
-            behaviour = cmp.ConfirmBehavior.Insert,
-            select = true
-          }),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<Tab>'] = cmp.mapping.disable
-        },
-        sources = {
-          { name = 'copilot' },
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path',    max_item_count = 10 },
-          { name = 'buffer',  max_item_count = 10 },
-        },
-        formatting = {
-          format = lspkind.cmp_format({
-            mode = 'symbol_text',
-            symbol_map = { Copilot = '' }
-          })
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        complete_opt = 'menu,menuone,noinsert',
-        experimental = {
-          ghost_text = true,
-        }
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
+        -- currently this is used to go to end of line
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-y>'] = cmp.mapping.confirm({
+          behaviour = cmp.ConfirmBehavior.Replace,
+          select = true
+        }),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<CR>'] = cmp.mapping.confirm({
+          behaviour = cmp.ConfirmBehavior.Insert,
+          select = true
+        }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<Tab>'] = cmp.mapping.disable
+      },
+      sources = {
+        { name = 'copilot' },
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'path',    max_item_count = 10 },
+        { name = 'buffer',  max_item_count = 10 },
+      },
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = 'symbol_text',
+          symbol_map = { Copilot = '' }
+        })
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      complete_opt = 'menu,menuone,noinsert',
+      experimental = {
+        ghost_text = true,
       }
-    end,
-  }
+    }
+  end,
 }
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -5,7 +5,6 @@ return {
   },
   config = function()
     local lspconfig = require('lspconfig')
-    local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
     local border = {
       { '╭', 'FloatBorder' },
@@ -26,7 +25,6 @@ return {
           ['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
           ['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
         },
-        capabilities = capabilites,
       }
     )
 
