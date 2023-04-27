508bd83a97a60da0b38d9424f94594f27641e451
Morgan McCauley <morgan@mccauley.co.nz>
2023-04-28 10:40:46 +1200

    Ensure dadbod preview height is number rather than float

--- nvim/lua/plugins/dadbod.lua
+++ nvim/lua/plugins/dadbod.lua
@@ -8,6 +8,6 @@ return {
     vim.g.db_ui_show_help = 0
     vim.g.db_ui_winwidth = 30
 
-    vim.o.previewheight = vim.fn.winheight(0) / 2
+    vim.o.previewheight = vim.fn.float2nr(vim.fn.winheight(0) / 2)
   end
 }
