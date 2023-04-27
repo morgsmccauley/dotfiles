6f3831351ace05f962d7979f31e73a6999e81c5f
Morgan McCauley <morgan@mccauley.co.nz>
2023-04-28 10:41:14 +1200

    Remove TS playground as it's now native

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,6 @@ return {
     dependencies = {
         'sheerun/vim-polyglot',
         'nvim-treesitter/nvim-treesitter-textobjects',
-        'nvim-treesitter/playground'
     },
     build = ':TSUpdate',
     config = function()
@@ -13,24 +12,6 @@ return {
                 enable = true,
                 use_languagetree = true,
                 additional_vim_regex_highlighting = false
-            },
-            playground = {
-                enable = true,
-                disable = {},
-                updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
-                persist_queries = false, -- Whether the query persists across vim sessions
-                keybindings = {
-                    toggle_query_editor = 'o',
-                    toggle_hl_groups = 'i',
-                    toggle_injected_languages = 't',
-                    toggle_anonymous_nodes = 'a',
-                    toggle_language_display = 'I',
-                    focus_language = 'f',
-                    unfocus_language = 'F',
-                    update = 'R',
-                    goto_node = '<cr>',
-                    show_help = '?',
-                },
             },
             textobjects = {
                 select = {
