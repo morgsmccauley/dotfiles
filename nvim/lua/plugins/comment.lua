4c8f352c6654ecab85b79e6fd19e89b4aec676e4
Morgan McCauley <morgan@mccauley.co.nz>
2023-05-03 18:28:48 +1200

    Use default `comment.nvim` maps

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,22 +1,7 @@
 return {
   'numToStr/Comment.nvim',
-  keys = {
-    {
-      '<C-/>',
-      function()
-        require('Comment.api').toggle.linewise.current()
-      end,
-    },
-    {
-      '<C-/>',
-      '<Plug>(comment_toggle_blockwise_visual)',
-      mode = 'v',
-      { expr = true }
-    }
-  },
+  keys = { 'gc', 'gb' },
   config = function()
-    require('Comment').setup({
-      mappings = false
-    })
+    require('Comment').setup({})
   end
 }
