c771e5332daa05e96c69d6898e4d89b043dd100d
Morgan McCauley <morgan@mccauley.co.nz>
2023-05-03 18:23:25 +1200

    Add tmux back in

--- nvim/lua/plugins/neovim-session-manager.lua
+++ nvim/lua/plugins/neovim-session-manager.lua
@@ -2,13 +2,13 @@ return {
   'Shatur/neovim-session-manager',
   config = function()
     require('session_manager').setup({
-      -- autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir,
+      autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir,
       max_path_length = 1,
       autosave_ignore_filetypes = {
         'Outline',
         'toggleterm'
       },
-      autosave_only_in_session = true
+      -- autosave_only_in_session = true
     })
   end
 }
