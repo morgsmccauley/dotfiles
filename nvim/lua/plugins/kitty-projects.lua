80f10df1c9636df3aa89a7a2616be13f243a15e9
Morgan McCauley <morgan@mccauley.co.nz>
2023-05-21 12:16:43 +1200

    Open neovim on kitty project open

--- nvim/lua/plugins/kitty-projects.lua
+++ nvim/lua/plugins/kitty-projects.lua
@@ -2,9 +2,10 @@ return {
   dir = '/Users/morganmccauley/Developer/Repositories/kitty-projects',
   config = function()
     require('kitty').setup({
-      command = 'nvim',
+      command = 'zsh --login -c nvim',
       workspaces = {
         { vim.env.HOME .. '/Developer/Repositories' },
+        { vim.env.HOME .. '/.local/share/nvim/lazy' },
         vim.env.HOME .. '/.dotfiles'
       }
     })
