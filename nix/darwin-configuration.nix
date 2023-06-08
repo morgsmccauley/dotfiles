27b005c30c3049fe4e5aac27e0e522799945783f
Morgan McCauley <morgan@mccauley.co.nz>
2023-06-08 20:37:31 +1200

    Remove zoxide nix package

--- nix/darwin-configuration.nix
+++ nix/darwin-configuration.nix
@@ -67,11 +67,6 @@
     enableFzfCompletion = true;
   };
 
-  package.zoxide = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
