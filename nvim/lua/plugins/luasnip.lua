b93ff753a6770f7b089589da477a02892ee01305
Morgan Mccauley <morgan@mccauley.co.nz>
2023-06-26 14:28:25 +1200

    Add dynamic luasnips

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -30,6 +30,12 @@ return {
   },
   config = function()
     local luasnip = require('luasnip')
+    local fmt = require('luasnip.extras.fmt').fmt
+
+    local snippet = luasnip.snippet
+    local text = luasnip.text_node
+    local insert = luasnip.insert_node
+    local fn = luasnip.function_node
 
     luasnip.setup({
       history = true,
@@ -39,5 +45,46 @@ return {
     luasnip.filetype_extend('typescript', { 'javascript' })
 
     require('luasnip.loaders.from_snipmate').lazy_load({ paths = { '~/.config/nvim/snippets' } })
+
+    local function get_variable_name()
+      local parser = vim.treesitter.get_parser(0, "javascript")
+      local query = vim.treesitter.query.parse("javascript", "(variable_declarator (identifier) @id)")
+
+      local cursor = vim.api.nvim_win_get_cursor(0)
+      local row = cursor[1]
+
+      for _, tree in pairs(parser:parse()) do
+        for _, match in query:iter_captures(tree:root(), 0, row - 2, row - 1) do
+          return vim.treesitter.get_node_text(match, 0)
+        end
+      end
+
+      return ''
+    end
+
+    luasnip.add_snippets('all', {
+      snippet(
+        'clv',
+        fmt(
+          [[console.log({{ {} }})]],
+          {
+            fn(get_variable_name)
+          }
+        )
+      ),
+      snippet(
+        "r",
+        fmt(
+          [[local {} = require('{}')]],
+          {
+            fn(function(args)
+              local parts = vim.split(args[1][1], '.', true)
+              return parts[#parts] or ''
+            end, { 1 }),
+            insert(1)
+          }
+        )
+      ),
+    })
   end,
 }
