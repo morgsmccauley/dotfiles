9df8e8d9d3edf4edbb02d09c966d3779c2f6917d
Morgan Mccauley <morgan@mccauley.co.nz>
2023-07-02 21:16:29 +1200

    Add all variables to log snippet

--- nvim/lua/plugins/luasnip/snippets/javascript.lua
+++ nvim/lua/plugins/luasnip/snippets/javascript.lua
@@ -1,17 +1,22 @@
 local function get_variable_name()
   local parser = vim.treesitter.get_parser(0, "javascript")
-  local query = vim.treesitter.query.parse("javascript", "(variable_declarator (identifier) @id)")
+  local query = vim.treesitter.query.parse("javascript", [[
+    ((identifier) @id)
+    ((shorthand_property_identifier_pattern) @id)
+  ]])
 
   local cursor = vim.api.nvim_win_get_cursor(0)
   local row = cursor[1]
 
+  local matches = {}
+
   for _, tree in pairs(parser:parse()) do
     for _, match in query:iter_captures(tree:root(), 0, row - 2, row - 1) do
-      return vim.treesitter.get_node_text(match, 0)
+      table.insert(matches, vim.treesitter.get_node_text(match, 0))
     end
   end
 
-  return ''
+  return table.concat(matches, ', ')
 end
 
 return {
@@ -20,7 +25,7 @@ return {
       dscr = 'console.log JSON',
     },
     fmt(
-      [[console.log(JSON.stringify({{ {} }}, null, 2))]],
+      [[console.log(JSON.stringify({{ {} }}, null, 2));]],
       {
         i(1)
       }
@@ -28,10 +33,10 @@ return {
   ),
   s({
       trig = 'clv',
-      decr = 'console.log variable'
+      dscr = 'console.log variable'
     },
     fmt(
-      [[console.log({{ {} }})]],
+      [[console.log({{ {} }});]],
       {
         f(get_variable_name)
       }
