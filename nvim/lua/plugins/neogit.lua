1eac3e07c9205c0edc5a57fac009fc19bd078f1b
Morgan Mccauley <morgan@mccauley.co.nz>
2023-08-04 20:36:54 +1200

    Enable neogit telescope integration

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -1,25 +1,30 @@
 return {
   'NeogitOrg/neogit',
+  -- dir = '~/Developer/neogit',
   cmd = 'Neogit',
   keys = {
     {
       '<C-g>',
       function()
+        -- ensure telescope-ui-select is loaded
+        require('telescope')
+
         if vim.bo.filetype == 'NeogitStatus' then
           vim.api.nvim_command('q')
         else
           vim.api.nvim_command('Neogit')
         end
       end,
-    }
+    },
   },
   config = function()
     require('neogit').setup({
       disable_commit_confirmation = true,
       disable_hint = true,
-      disable_insert_on_commit = false,
+      disable_insert_on_commit = "auto",
       integrations = {
-        diffview = true
+        diffview = true,
+        -- telescope = false
       }
     })
   end,
