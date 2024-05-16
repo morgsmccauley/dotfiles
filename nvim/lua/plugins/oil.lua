2f243859c4f5522d85d5e358a6e97d5ee8ea6177
Morgan Mccauley <morgan@mccauley.co.nz>
2024-05-17 10:07:20 +1200

    Update oil maps

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,6 +1,27 @@
 return {
   'stevearc/oil.nvim',
-  opts = {},
-  -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require 'oil'.setup({
+      keymaps = {
+        ["<CR>"] = "actions.select",
+        ["<C-l>"] = "actions.select",
+        ["<C-v>"] = "actions.select_vsplit",
+        ["<C-s>"] = false,
+        -- ["<C-h>"] = "actions.select_split",
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["<C-q>"] = "actions.close",
+        ["<C-r>"] = "actions.refresh",
+        ["<C-h>"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      }
+    })
+  end
 }
