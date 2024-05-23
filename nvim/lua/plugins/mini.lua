49168779ceceec39ccd84385bce23915e023e4c4
Morgan Mccauley <morgan@mccauley.co.nz>
2024-05-23 13:34:09 +1200

    Add mini keymaps

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -19,7 +19,28 @@ return {
     'echasnovski/mini.files',
     version = '*',
     config = function()
-      require('mini.files').setup()
+      local mini_files = require('mini.files')
+
+      local files_set_cwd = function(path)
+        local cur_entry_path = mini_files.get_fs_entry().path
+        local cur_directory = vim.fs.dirname(cur_entry_path)
+        vim.fn.chdir(cur_directory)
+      end
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MiniFilesBufferCreate',
+        callback = function(args)
+          vim.keymap.set('n', 'g~', files_set_cwd, { buffer = args.buf })
+        end,
+      })
+
+      require('mini.files').setup({
+        mappings = {
+          go_out_plus = 'h',
+          go_in_plus = 'l',
+          close = '<C-q>'
+        }
+      })
     end
   }
 }
