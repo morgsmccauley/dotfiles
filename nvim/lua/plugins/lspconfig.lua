95ad34080963d663dd6ef6fef4ff4b79c5984252
Morgan Mccauley <morgan@mccauley.co.nz>
2024-05-28 15:12:19 +1200

    Replace null-ls jq with native lsp

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -79,7 +79,10 @@ return {
       end
     }
 
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
     lspconfig.jsonls.setup {
+      capabilities,
       settings = {
         json = {
           schemas = require('schemastore').json.schemas(),
--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,22 +1,19 @@
 return {
+  enabled = false,
   'jose-elias-alvarez/null-ls.nvim',
   dependencies = {
     'jay-babu/mason-null-ls.nvim',
   },
   config = function()
     local null_ls = require('null-ls')
-    require('null-ls').setup({
+
+    null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.jq,
         null_ls.builtins.formatting.prettierd,
       }
     })
 
     -- needs to be after null-ls setup
-    require('mason-null-ls').setup({
-      automatic_installation = {
-        exclude = { 'jq' }
-      },
-    })
+    require('mason-null-ls').setup()
   end
 }
