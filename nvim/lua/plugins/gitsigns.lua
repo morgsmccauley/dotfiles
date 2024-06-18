b22b0bebfda9fbc4c62d794ff0559beb090586af
Morgan Mccauley <morgan@mccauley.co.nz>
2024-06-18 20:30:03 +1200

    Update gitsigns symbol

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -2,11 +2,6 @@ return {
   'lewis6991/gitsigns.nvim',
   config = function()
     require('gitsigns').setup {
-      signs = {
-        add = { text = '▌' },
-        change = { text = '▌' },
-        untracked = { text = '▌' },
-      },
       watch_gitdir = {
         interval = 100
       },
