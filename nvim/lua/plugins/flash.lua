1724c395f6d6e259d4e8574a601ed63561cda1db
Morgan Mccauley <morgan@mccauley.co.nz>
2024-07-08 10:34:56 +1200

    Update plugins

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,13 +1,6 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
-  opts = {
-    modes = {
-      search = {
-        enabled = true
-      }
-    }
-  },
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
