7a355155f4bf60b7eb14c42bfcf4e7338c1d4c91
Morgan Mccauley <morgan@mccauley.co.nz>
2025-01-11 19:53:32 +1300

    Add github and cheats

--- nix/modules/home/programs/gh.nix
+++ nix/modules/home/programs/gh.nix
@@ -1,5 +1,14 @@
 { ... }: {
   programs.gh = {
     enable = true;
+    settings = {
+      aliases = {
+        po = "pr view --web"
+        wr = "workflow run"
+        wl = "workflow list"
+        rw = "run watch"
+        rl = "run view --log"
+      };
+    };
   };
 }
