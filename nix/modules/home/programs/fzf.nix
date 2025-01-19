6921928243595296953da0b39d3dae975f93ed4c
Morgan Mccauley <morgan@mccauley.co.nz>
2025-01-20 11:17:14 +1300

    Add fzf colors

--- nix/modules/home/programs/fzf.nix
+++ nix/modules/home/programs/fzf.nix
@@ -7,21 +7,21 @@
       "--bind ctrl-u:preview-page-up,ctrl-d:preview-page-down"
     ];
 
-    # Catppuccin-Macchiato
-    colors = {
-      "bg" = "#24273a";
-      "bg+" = "#24273a";
-      "spinner" = "#f4dbd6";
-      "hl" = "#ed8796";
-      "fg" = "#cad3f5";
-      "header" = "#ed8796";
-      "info" = "#c6a0f6";
-      "pointer" = "#f4dbd6";
-      "marker" = "#f4dbd6";
-      "fg+" = "#cad3f5";
-      "prompt" = "#c6a0f6";
-      "hl+" = "#ed8796";
-    };
+    # # Catppuccin-Macchiato
+    # colors = {
+    #   "bg" = "#24273a";
+    #   "bg+" = "#24273a";
+    #   "spinner" = "#f4dbd6";
+    #   "hl" = "#ed8796";
+    #   "fg" = "#cad3f5";
+    #   "header" = "#ed8796";
+    #   "info" = "#c6a0f6";
+    #   "pointer" = "#f4dbd6";
+    #   "marker" = "#f4dbd6";
+    #   "fg+" = "#cad3f5";
+    #   "prompt" = "#c6a0f6";
+    #   "hl+" = "#ed8796";
+    # };
 
     # # Catppuccin-Latte
     # colors = {
@@ -38,5 +38,22 @@
     #   "prompt" = "#8839ef";
     #   "hl+" = "#d20f39";
     # };
+  
+    # Catppuccin-Mocha
+    colors = {
+      "bg" = "#1e1e2e";
+      "bg+" = "#313244";
+      "spinner" = "#f5e0dc";
+      "hl" = "#f38ba8";
+      "fg" = "#cdd6f4";
+      "header" = "#f38ba8";
+      "info" = "#cba6f7";
+      "pointer" = "#f5e0dc";
+      "marker" = "#b4befe";
+      "fg+" = "#cdd6f4";
+      "prompt" = "#cba6f7";
+      "hl+" = "#f38ba8";
+      "selected-bg" = "#45475a";
+    };
   };
 }
