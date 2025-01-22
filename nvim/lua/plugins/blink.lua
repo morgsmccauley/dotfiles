51dbba1d3ed866545f3ba34f2fd60db79fc66936
Morgan Mccauley <morgan@mccauley.co.nz>
2025-01-22 14:20:00 +1300

    Add copilot to blink

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -20,7 +20,7 @@ return {
       use_nvim_cmp_as_default = true,
       -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono'
+      nerd_font_variant = 'mono',
     },
 
     completion = {
@@ -32,22 +32,35 @@ return {
       -- fuzzy type to get what I am looking for, and then accept. When I don't know what I am looking for then I can open up
       -- this could work well with copilot too so its not too busy having both
       menu = {
-        auto_show = true
+        auto_show = true,
+        draw = {
+          columns = { { "label", "label_description", gap = 1 }, { "kind" } }
+        }
       },
       ghost_text = {
         enabled = true
-      }
-      -- menu = {
-      --   draw = {
-      --     columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } }
-      --   }
-      -- }
+      },
     },
 
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
+      providers = {
+        copilot = {
+          name = 'copilot',
+          module = 'blink-cmp-copilot',
+          score_offset = 100,
+          async = true,
+          transform_items = function(_, items)
+            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+            local kind_idx = #CompletionItemKind + 1
+            CompletionItemKind[kind_idx] = "Copilot"
+            for _, item in ipairs(items) do
+              item.kind = kind_idx
+            end
+            return items
+          end,
+        }
+      }
     },
     signature = { enabled = true }
   },
--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -2,15 +2,8 @@ return {
   'zbirenbaum/copilot.lua',
   cmd = 'Copilot',
   event = 'InsertEnter',
-  enabled = false,
   dependencies = {
-
-    {
-      'zbirenbaum/copilot-cmp',
-      config = function()
-        require('copilot_cmp').setup()
-      end
-    }
+    'giuxtaposition/blink-cmp-copilot',
   },
   config = function()
     require('copilot').setup({
