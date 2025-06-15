8a6691c4a97858088bed56f63177ef50ad3e418f
Morgan Mccauley <morgan@mccauley.co.nz>
2025-06-15 22:11:19 +1200

    Update kitty config

--- nvim/lua/plugins/kitty-projects.lua
+++ nvim/lua/plugins/kitty-projects.lua
@@ -1,17 +1,15 @@
 return {
-  'morgsmccauley/kitty-projects.nvim',
+  dir = '~/Developer/kitty-projects.nvim',
   config = function()
     require('kitty').setup({
       command = 'zsh --login -c /etc/profiles/per-user/morganmccauley/bin/nvim',
       project_paths = {
+        { vim.env.HOME .. '/Developer/monorepo',    exclude_hidden = true },
         { vim.env.HOME .. '/Developer' },
         { vim.env.HOME .. '/.local/share/nvim/lazy' },
-        vim.env.HOME .. '/Developer/monorepo/monorepo-main',
-        vim.env.HOME .. '/Developer/monorepo/monorepo-dev',
-        vim.env.HOME .. '/Developer/monorepo/monorepo-review',
-        vim.env.HOME .. '/Developer/monorepo/monorepo-scratch',
         vim.env.HOME .. '/.dotfiles'
-      }
+      },
+      picker = 'snacks'
     })
   end
 }
