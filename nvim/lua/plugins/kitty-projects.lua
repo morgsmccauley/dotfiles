874952fdbd05e3a45fbf7cb92c600753adcad234
Morgan Mccauley <morgan@mccauley.co.nz>
2025-07-18 14:03:04 +1200

    add doc folder to kitty projects

--- nvim/lua/plugins/kitty-projects.lua
+++ nvim/lua/plugins/kitty-projects.lua
@@ -6,9 +6,8 @@ return {
       project_paths = {
         { vim.env.HOME .. '/Developer/monorepo',    exclude_hidden = true },
         { vim.env.HOME .. '/Developer' },
+        { vim.env.HOME .. '/Documents' },
         { vim.env.HOME .. '/.local/share/nvim/lazy' },
-        vim.env.HOME .. '/Documents/tax',
-        vim.env.HOME .. '/Documents/budget',
         vim.env.HOME .. '/.dotfiles'
       },
       picker = 'snacks'
