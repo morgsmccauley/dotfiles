3a33fe4eff134e698c66e1a2c213775e6d6bd2b0
Morgan Mccauley <morgan@mccauley.co.nz>
2026-02-10 11:24:42 +1300

    feat: add global post-checkout hook for nvim multiplexer branch metadata
    Also migrate deprecated git options (userName, userEmail, aliases) to
    their new locations under settings.

--- nix/modules/home/programs/git.nix
+++ nix/modules/home/programs/git.nix
@@ -2,20 +2,20 @@
   programs.git = {
     enable = true;
 
-    userName = "Morgan Mccauley";
-    userEmail = "morgan@mccauley.co.nz";
-
-    aliases = {
-      w = "worktree";
-      st = "status --short";
-      l = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-    };
-
     hooks = {
       post-checkout = ./git/post-checkout;
     };
 
     settings = {
+      user = {
+        name = "Morgan Mccauley";
+        email = "morgan@mccauley.co.nz";
+      };
+      alias = {
+        w = "worktree";
+        st = "status --short";
+        l = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      };
       core = {
         editor = "nvim";
         pager = "less";
